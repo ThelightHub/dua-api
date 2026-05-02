7b10791aa4fdea8e5437c2f9e7532462af25f81e
ThelightHub <hisnulm88@gmail.com>
2026-05-02 22:26:59 +0600

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
+export interface Env {
+  DB: D1Database;
+}
 
+export default {
+  async fetch(request, env): Promise<Response> {
+    const { pathname } = new URL(request.url);
+
+    if (pathname === "/api/beverages") {
+      // If you did not use `DB` as your binding name, change it here
+      const { results } = await env.prod_d1_tutorial.prepare(
+        "SELECT * FROM Customers WHERE CompanyName = ?",
+      )
+        .bind("Bs Beverages")
+        .run();
+      return Response.json(results);
+    }
+
+    return new Response(
+      "Call /api/beverages to see everyone who works at Bs Beverages",
+    );
+  },
+} satisfies ExportedHandler<Env>;
